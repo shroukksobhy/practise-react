4c2600ab43066f1e5ded1fc1a90e88f9fce5ec7e
ShroukInituim <73167625+ShroukInituim@users.noreply.github.com>
2021-04-21 15:00:01 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,50 +1,84 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import React from "react";
-import Navbar from "./components/Navbar";
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [], // data from api
-      isLoaded: false, // if data is loaded from the api
-    };
-  }
-  componentDidMount() {
-    fetch('https://api.covid19api.com/')
-    .then(res => res.json())
-    .then(json => {
-      this.setState({
-        isLoaded: true,
-        items: json
-      })
-    });
-  }
-  render() {
-    this.state = [
-      { name: "S", age: "12", address: "Shoubra Misr" },
-      { name: "H", age: "73", address: "Shoubra Misr" },
-      { name: "S", age: "74", address: "Shoubra Misr" },
-    ];
-    let {isLoaded,items} = this.state;
-    if(!isLoaded){
-      return <div>Loading...</div>
-    }else{
-      return (
-        <div className="container">
-          <Navbar />
-          {this.state.map((userInfo) => {
-            return (
-              <div>
-                <h1>{userInfo.name}</h1>
-                <span>{userInfo.age}</span>
-              </div>
-            );
-          })}
+import List from "./components/List";
+import withListLoading from "./components/withListLoading";
+import Navbar from './components/Navbar';
+function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `https://api.github.com/users/shroukksobhy/repos`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((repos) => {
+        setAppState({ loading: false, repos: repos });
+      });
+  }, [setAppState]);
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="container">
+        <h1 className="title">My Repositories</h1>
+        <div className="repo-container">
+          <ListLoading isLoading={appState.loading} repos={appState.repos} />
         </div>
-      );
-    }
-   
-  }
+      </div>
+    </div>
+  );
 }
 export default App;
+
+// import logo from "./logo.svg";
+// import "./App.css";
+// import React, { useEffect, useState } from 'react'
+// import Navbar from "./components/Navbar";
+// import List from './components/List';
+// import withListLoading from './components/withListLoading';
+
+// }
+// class App extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       items: {}, // data from api
+//       isLoaded: false, // if data is loaded from the api
+//     };
+//   }
+//   componentDidMount() {
+//     fetch("https://api.github.com/users/shroukksobhy")
+//       .then((response) => response.json())
+//       .then((data) => console.log("This is your data", data))
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }
+
+//   render() {
+//     const { isLoaded, items } = this.state;
+//     if (!isLoaded) {
+//       return (
+//         <div><Navbar/>Loading...</div>
+//       );
+//     } else {
+//       return (
+//         <div className="container">
+//           Data has been loaded
+//           <ul>
+//             {items.map((item) => (
+//               <li key={item.Country}>{item.Country}</li>
+//             ))}
+//             ;
+//           </ul>
+//           <Navbar />
+//         </div>
+//       );
+//     }
+//   }
+// }
+// export default App;
---
+++ src/components/List.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// class List extends React.Component{
+//     render(){
+//         const{repos}  = props;
+//         if(!repos || repo.length === 0){
+//             return <p>No repos are found, Sorry!</p>
+//         }
+//         return (
+
+//         );
+//     };
+// }
+const List = (props) => {
+  const { repos } = props;
+  if (!repos || repos.length === 0) return <p>No repos, sorry</p>;
+  return (
+    <ul>
+      <h2 className='list-head'>Available Public Repositories</h2>
+      {repos.map((repo) => {
+        return (
+          <li key={repo.id} className='list'>
+            <span className='repo-text'>{repo.name} </span>
+            <span className='repo-description'>{repo.description}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+export default List;
